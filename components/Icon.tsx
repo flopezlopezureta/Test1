314e52270527da9f1daa171a303c22dfaf631727
flopezlopezureta <flopez@selcom.cl>
2026-04-30 02:01:31 -0400

    fix: add missing IconExternalLink to fix build error

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -719,3 +719,12 @@ export const IconPieChart: React.FC<IconProps> = ({ className, title }) => (
         <path d="M22 12A10 10 0 0 0 12 2v10z"></path>
     </svg>
 );
+
+export const IconExternalLink: React.FC<IconProps> = ({ className, title }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
+        {title && <title>{title}</title>}
+        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+        <polyline points="15 3 21 3 21 9"></polyline>
+        <line x1="10" y1="14" x2="21" y2="3"></line>
+    </svg>
+);
