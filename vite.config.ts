3106ff73ead237cfd8ebca2d2d257f2999c5091f
flopezlopezureta <flopez@selcom.cl>
2026-07-11 19:00:06 -0400

    chore: split build into manual chunks and increase max memory to 1024MB, bump version to 2.6.77

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,26 @@ export default defineConfig({
   plugins: [react()],
   define: {
     'import.meta.env.VITE_APP_VERSION': JSON.stringify(packageJson.version)
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('recharts') || id.includes('d3')) {
+              return 'vendor-charts';
+            }
+            if (id.includes('exceljs') || id.includes('xlsx') || id.includes('fast-csv')) {
+              return 'vendor-excel';
+            }
+            if (id.includes('framer-motion')) {
+              return 'vendor-motion';
+            }
+            return 'vendor-core'; // other third-party dependencies
+          }
+        }
+      }
+    }
   }
 })
